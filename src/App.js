fcc06f9638d552701a9cce7f8f3a4e493fc83a54
Jason Chen <jasonccfu@Jasons-MacBook-Pro.local>
2023-05-12 10:50:29 -0700

    start to code from here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from "./components/Navbar"
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+
 
 function App() {
   return (
-    <Nav></Nav>
+    <div></div>
  
   );
 }
