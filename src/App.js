3761627c874b4de163ae44e3314227b9efec60f7
Jason Chen <jasonccfu@Jasons-MacBook-Pro.local>
2023-05-12 13:15:13 -0700

    profile.js and install react-typed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Profile />
     </div>
  
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,6 @@ const Navbar = () => {
       <div className='block md:hidden' onClick={handleNav} > 
         {!nav?<AiOutlineClose size={20}/>: <AiOutlineMenu size={20}/>}
       </div>
-
       <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gary-900 bg-[#000300] ease-out duration-500': 'fixed left-[-100%]' }>
       <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
         <ul className='uppercase p-4'>
