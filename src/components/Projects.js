8b104e199441c4f6fd7cb763f4501aac1a185d0b
Jasonccfu <jason.ccfu@gmail.com>
2023-05-15 15:14:02 -0700

    fixed the bug with navbar menu showing when mobile size

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,7 +18,7 @@ const Projects = () => {
                     <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">Check it out!</button>
                 </div>
                  {/* secend card */}
-                 <div className="w-full shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
+                 <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
                     <img className="w-20 mx-auto mt-[-3rem] bg-white" src={Project} alt="/" />
                     <h2 className="text-2xl font-bold text-center py-8">Project-1</h2>
                     <p className="text-center text-4xl font-bold">This test</p>
